26e03fc33d9b08b57b361668cae2009f61ceb71c
Moussa Diatta <diattamoussa010@gmail.com>
2025-10-31 09:32:20 +0000

    faire les notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_notifications(request):
+    notifications = Notification.objects.filter(recipient=request.user)
+    serializer = NotificationSerializer(notifications, many=True)
+    return Response(serializer.data)
+
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def mark_as_read(request, notification_id):
+    try:
+        notification = Notification.objects.get(id=notification_id, recipient=request.user)
+    except Notification.DoesNotExist:
+        return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    notification.is_read = True
+    notification.save()
+    return Response({'message': 'Marked as read'})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def unread_count(request):
+    count = Notification.objects.filter(recipient=request.user, is_read=False).count()
+    return Response({'unread_count': count})
